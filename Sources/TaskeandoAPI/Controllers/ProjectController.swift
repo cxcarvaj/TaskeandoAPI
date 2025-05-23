acecaa7a557383f59dd33a7682fa49901dbb0c1a
cxcarvaj <cxcarvaj@gmail.com>
2025-05-23 08:28:26 -0500

    Adding ProjectController to our app

---
+++ Sources/TaskeandoAPI/Controllers/ProjectController.swift
@@ -0,0 +1,105 @@
+//
+//  ProjectController.swift
+//  TaskeandoAPI
+//
+//  Created by Carlos Xavier Carvajal Villegas on 22/5/25.
+//
+
+
+import Vapor
+import Fluent
+
+struct ProjectController: RouteCollection {
+    func boot(routes: any RoutesBuilder) throws {
+        let api = routes.grouped("api")
+        
+        let secureToken = api.grouped(UserTokens.authenticator(), Users.guardMiddleware())
+        secureToken.group("project") { group in
+            group.post(use: createProject)
+            group.get(use: getProjects)
+            group.get(":projectID", use: getProject)
+            group.get("allProjects", use: getProjectsAdmin)
+            group.get(":projectID", "admin", use: getProjectsAdmin)
+        }
+    }
+    
+    func createProject(req: Request) async throws -> HTTPStatus {
+        let user = try req.auth.require(Users.self)
+        let newProject = try req.content.decode(Projects.self)
+        if let project = try await Projects.find(newProject.id, on: req.db) {
+            if try await project.$users.query(on: req.db)
+                .filter(\.$id == user.requireID())
+                .first() != nil || user.role == .admin {
+                project.name = newProject.name
+                project.summary = newProject.summary
+                project.type = newProject.type
+                project.state = newProject.state
+                try await project.update(on: req.db)
+            }
+        } else {
+            try await newProject.create(on: req.db)
+            try await newProject.$users.attach(user, on: req.db)
+        }
+        return .accepted
+    }
+    
+    func getProjects(req: Request) async throws -> [Projects] {
+        let user = try req.auth.require(Users.self)
+        return try await user.$projects
+            .query(on: req.db)
+            .with(\.$tags)
+            .with(\.$tasks)
+            .with(\.$users)
+            .all()
+    }
+    
+    func getProject(req: Request) async throws -> Projects {
+        let user = try req.auth.require(Users.self)
+        let id = try req.parameters.require("projectID", as: UUID.self)
+        if let project = try await user.$projects
+            .query(on: req.db)
+            .filter(\.$id == id)
+            .with(\.$tags)
+            .with(\.$tasks)
+            .with(\.$users)
+            .first() {
+            return project
+        } else {
+            throw Abort(.notFound)
+        }
+    }
+    
+    func getProjectAdmin(req: Request) async throws -> Projects {
+        let user = try req.auth.require(Users.self)
+        let id = try req.parameters.require("projectID", as: UUID.self)
+        if user.role == .admin {
+            if let project = try await Projects
+                .query(on: req.db)
+                .filter(\.$id == id)
+                .with(\.$tags)
+                .with(\.$tasks)
+                .with(\.$users)
+                .first() {
+                return project
+            } else {
+                throw Abort(.notFound)
+            }
+        } else {
+            throw Abort(.forbidden)
+        }
+    }
+    
+    func getProjectsAdmin(req: Request) async throws -> [Projects] {
+        let user = try req.auth.require(Users.self)
+        if user.role == .admin {
+            return try await Projects
+                .query(on: req.db)
+                .with(\.$tags)
+                .with(\.$tasks)
+                .with(\.$users)
+                .all()
+        } else {
+            throw Abort(.forbidden)
+        }
+    }
+}
--- Sources/TaskeandoAPI/routes.swift
+++ Sources/TaskeandoAPI/routes.swift
@@ -3,4 +3,5 @@ import Vapor
 
 func routes(_ app: Application) throws {
     try app.register(collection: UsersController())
+    try app.register(collection: ProjectController())
 }
